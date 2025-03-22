3fd429e578455f5f38d0de471d2ba28d2bca52ae
pedram <pedram.najafi73@gmail.com>
2025-03-22 23:33:25 +0330

    create utils for docker container run,remove and add

---
+++ tests/utils/docker_utils.py
@@ -0,0 +1,59 @@
+import docker
+import time
+import docker.errors
+
+
+def is_container_ready(container):
+    container.reload()
+    return container.status == "running"
+
+def wait_for_stable_status(container,stable_duration=3,interval=1):
+    start_time = time.time()
+    stable_count = 0
+    while time.time() - start_time < stable_duration:
+        if is_container_ready(container):
+            stable_count += 1 
+        else:
+            stable_count = 0 
+        
+        if stable_count >= stable_duration / interval:
+            return True
+        
+        time.sleep(interval)
+    return False
+
+
+def start_database_container():
+    client = docker.from_env()
+    container_name = "test-db"
+
+    try:
+        existing_container =client.containers.get(container_name)
+        print(f"container {container_name} exists. stoping and removing...")
+        existing_container.stop()
+        existing_container.remove()
+        print(f"container {container_name} stopped and removed")
+
+    except docker.errors.NotFound:
+        print(f"container {container_name} does not exists")
+
+    # container configuration 
+    container_config = {    
+        "name" : container_name,
+        "image": "postgres:16.1-alpine3.19",
+        "detach" : True,
+        "ports":{"5432":"5433"},
+        "environment": {
+            "POSTGRES_USER": "postgres",
+            "POSTGRES_PASSWORD": "postgres",
+        },
+    }
+
+    # start container
+    container = client.containers.run(**container_config)
+    while not is_container_ready(container):
+        time.sleep(1)
+
+    if not wait_for_stable_status(container):
+        raise RuntimeError("container did not stabilize whitin the specified time")
+    return container
